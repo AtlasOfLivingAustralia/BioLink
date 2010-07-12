2b0bec3655bba6ae435255eef13b5d4902d24aa4
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2010-07-12 08:04:24 +0000

    First cut of Taxa plugin (prototype)

---
+++ BioLink.Taxa/TaxaPlugin.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BioLink.Client.Extensibility;
+
+namespace BioLink.Taxa {
+
+    public class TaxaPlugin : IBioLinkPlugin {
+
+        public string Name {
+            get { return "Taxa"; }
+        }
+
+
+
+        public List<IWorkspaceContribution> Contributions {
+            get {
+                List<IWorkspaceContribution> contrib = new List<IWorkspaceContribution>();
+
+                contrib.Add(new WorkspaceMenuContribution((obj, e) => { Debug.Log("here in Foo!"); },                     
+                    "{'Name':'Taxa', 'Header':'_Taxa','InsertAfter':'File'}", 
+                    "{'Name':'Foo', 'Header':'_Foo!'}"
+                ));
+
+                contrib.Add(new WorkspaceMenuContribution((obj, e) => { Debug.Log("here in Bar!"); },
+                    "Taxa",
+                    "{'Name':'Bar', 'Header':'_Bar!', 'SeparatorBefore':true}"
+                ));
+
+                return contrib;            
+            }
+        }
+    }
+}
