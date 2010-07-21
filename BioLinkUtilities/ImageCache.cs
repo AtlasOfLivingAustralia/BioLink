4ce52f5b522c1bcd9664fbc489ba7b437dad64cf
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2010-07-21 01:02:09 +0000

    Added some ignores, and model files missed in the last commit

---
+++ BioLinkUtilities/ImageCache.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace BioLink.Client.Utilities {
+
+    public static class ImageCache {
+
+        private static Dictionary<String, BitmapSource> _cache = new Dictionary<string, BitmapSource>();
+
+        public static BitmapSource GetImage(string uri) {
+
+            if (_cache.ContainsKey(uri)) {
+                return _cache[uri];
+            }
+
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            image.UriSource = new Uri(uri);
+            image.EndInit();
+            _cache.Add(uri, image);
+
+            return image;
+        }
+
+        public static BitmapSource ApplyOverlay(BitmapSource image, string overlayUri) {
+            BitmapSource overlay = GetImage(overlayUri);
+            int height = (int)image.Height;
+            int width = (int)image.Width;
+            RenderTargetBitmap bmp = new RenderTargetBitmap(width, height, image.DpiX, image.DpiY, PixelFormats.Pbgra32);
+            DrawingVisual drawingVisual = new DrawingVisual();
+            DrawingContext drawingContext = drawingVisual.RenderOpen();
+            drawingContext.DrawImage(image, new Rect(new Point(0, 0), new Point(width, height)));
+            drawingContext.DrawImage(overlay, new Rect(new Point(0, 0), new Point(width, height)));
+            drawingContext.Close();
+            bmp.Render(drawingVisual);
+            return bmp;
+        }
+
+    }
+
+}
---
+++ BioLinkUtilities/ViewModelBase.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Windows.Media.Imaging;
+
+namespace BioLink.Client.Utilities {
+
+    public abstract class ChangeableModelBase : IChangeable, INotifyPropertyChanged {
+
+        private bool _changed;
+
+        protected bool SetProperty<T>(string propertyName, T backingField, T value) {
+            var changed = !EqualityComparer<T>.Default.Equals(backingField, value);
+            if (changed) {
+                backingField = value;
+                RaisePropertyChanged(propertyName);
+                if (!SuspendChangeMonitoring) {
+                    IsChanged = true;
+                }
+            }
+            return changed;
+        }
+
+        protected bool SetProperty<T>(string propertyName, ref T backingField, T value) {
+            var changed = !EqualityComparer<T>.Default.Equals(backingField, value);
+            if (changed) {
+                backingField = value;
+                RaisePropertyChanged(propertyName);
+                if (!SuspendChangeMonitoring) {
+                    IsChanged = true;
+                }
+            }
+            return changed;
+        }
+
+        public bool IsChanged {
+            get { return _changed; }
+            set { 
+                SetProperty("IsChanged", ref _changed, value);
+                if (value && DataChanged != null) {
+                    DataChanged();                    
+                }
+            }
+        }
+
+        protected void RaisePropertyChanged(string propertyName) {
+            if (PropertyChanged != null) {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+
+        public bool SuspendChangeMonitoring { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public event DataChangedHandler DataChanged;
+    }
+
+    public delegate void DataChangedHandler();
+
+    public abstract class ViewModelBase : ChangeableModelBase {
+
+        public bool IsSelected { get; set; }
+
+        public abstract string Label { get; }
+        public abstract BitmapSource Icon { get; set; }
+    }
+
+}
