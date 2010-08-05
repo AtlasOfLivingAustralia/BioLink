bca9ed07ae9468c017d1ece5789c1224e2d23809
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2010-08-05 08:38:00 +0000

    added missing files for rename

---
+++ BioLink.Client.Extensibility/EditableTextBlock.xaml.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace BioLink.Client.Extensibility {
+
+    public partial class EditableTextBlock : UserControl {
+
+        #region Constructor
+
+        public EditableTextBlock() {
+            InitializeComponent();
+            base.Focusable = true;
+            base.FocusVisualStyle = null;
+        }
+
+        #endregion Constructor
+
+        #region Member Variables
+
+        // We keep the old text when we go into editmode
+        // in case the user aborts with the escape key
+        private string oldText;
+
+        #endregion Member Variables
+
+        #region Properties
+
+        public string Text {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(EditableTextBlock), new FrameworkPropertyMetadata("", FrameworkPropertyMetadataOptions.Journal | FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, new PropertyChangedCallback(OnTextChanged)));
+
+        public bool IsEditable {
+            get { return (bool)GetValue(IsEditableProperty); }
+            set { 
+                SetValue(IsEditableProperty, value);
+            }
+        }
+        public static readonly DependencyProperty IsEditableProperty = DependencyProperty.Register("IsEditable", typeof(bool), typeof(EditableTextBlock), new PropertyMetadata(true));
+
+        private bool _cancelled = false;
+
+        public bool IsInEditMode {
+            get {
+                _cancelled = false;
+                if (IsEditable) {
+                    return (bool)GetValue(IsInEditModeProperty);
+                } else {
+                    return false;
+                }
+            }
+            set {
+                if (IsEditable) {
+                    if (value) {
+                        oldText = Text;
+                    }
+                    SetValue(IsInEditModeProperty, value);
+                }
+            }
+        }
+
+        public static readonly DependencyProperty IsInEditModeProperty = DependencyProperty.Register("IsInEditMode", typeof(bool), typeof(EditableTextBlock), new PropertyMetadata(false));
+
+        public string TextFormat {
+            get { return (string) GetValue(TextFormatProperty); }
+            set {
+                if (value == "") {
+                    value = "{0}";
+                }
+                SetValue(TextFormatProperty, value);
+            }
+        }
+
+        public static readonly RoutedEvent TextChangedEvent = EventManager.RegisterRoutedEvent("TextChanged", RoutingStrategy.Bubble, typeof(RoutedPropertyChangedEventHandler<string>), typeof(EditableTextBlock));
+        
+        private static void OnTextChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args) {
+            if (args.OldValue != null) {
+                EditableTextBlock control = (EditableTextBlock)obj;
+                if (control.IsInEditMode) {
+                    RoutedPropertyChangedEventArgs<string> e = new RoutedPropertyChangedEventArgs<string>((string)args.OldValue, (string)args.NewValue, TextChangedEvent);
+                    control.OnTextChanged(e);
+                } else {
+                    (control as EditableTextBlock).Text = args.NewValue as string;
+
+                    TextBox txtBox = FindVisualChild<TextBox>(control);
+                    if (txtBox != null) {
+                        txtBox.Text = (string)args.NewValue;
+                    }
+
+                    TextBlock txt = FindVisualChild<TextBlock>(control);
+                    if (txt != null) {
+                        txt.Text = (string)args.NewValue;
+                    }
+                }
+            }
+        }
+
+        public static childItem FindVisualChild<childItem>(DependencyObject obj) where childItem : DependencyObject {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++) {
+                DependencyObject child = VisualTreeHelper.GetChild(obj, i);
+
+                if (child != null && child is childItem)
+                    return (childItem)child;
+                else {
+                    childItem childOfChild = FindVisualChild<childItem>(child);
+                    if (childOfChild != null)
+                        return childOfChild;
+                }
+            }
+
+            return null;
+        }
+
+        protected virtual void OnTextChanged(RoutedPropertyChangedEventArgs<string> args) {
+            RaiseEvent(args);
+        }
+
+        public static readonly DependencyProperty TextFormatProperty = DependencyProperty.Register( "TextFormat", typeof(string), typeof(EditableTextBlock), new PropertyMetadata("{0}"));
+
+        public string FormattedText {
+            get { return String.Format(TextFormat, Text); }
+        }
+
+        public object ViewModel {
+            get { return GetValue(ViewModelProperty); }
+            set { 
+                SetValue(ViewModelProperty, value);                
+            }
+        }
+
+        public static readonly DependencyProperty ViewModelProperty = DependencyProperty.Register("ViewModel", typeof(object), typeof(EditableTextBlock), new PropertyMetadata(null));
+
+        public TextDecorationCollection TextDecorations {
+            get { return GetValue(TextDecorationsProperty) as TextDecorationCollection; }
+            set { 
+                SetValue(TextDecorationsProperty, value);                 
+            }             
+        }
+
+        public static readonly DependencyProperty TextDecorationsProperty = DependencyProperty.Register("TextDecorations", typeof(TextDecorationCollection), typeof(EditableTextBlock), new PropertyMetadata(new TextBlock().TextDecorations));
+
+        #endregion Properties
+
+
+        #region Event Handlers
+
+        // Invoked when we enter edit mode.
+        void TextBox_Loaded(object sender, RoutedEventArgs e) {
+            TextBox txt = sender as TextBox;
+
+            // Give the TextBox input focus
+            txt.Focus();
+
+            txt.SelectAll();
+        }
+
+        // Invoked when we exit edit mode.
+        void TextBox_LostFocus(object sender, RoutedEventArgs e) {
+            this.IsInEditMode = false;
+            if (_cancelled) {
+                if (EditingCancelled != null) {
+                    EditingCancelled(this, oldText);
+                }
+            } else {
+                if (EditingComplete != null) {
+                    EditingComplete(this, Text);
+                }
+            }
+        }
+
+        // Invoked when the user edits the annotation.
+        void TextBox_KeyDown(object sender, KeyEventArgs e) {
+            if (e.Key == Key.Enter) {
+                this.IsInEditMode = false;
+                e.Handled = true;
+            } else if (e.Key == Key.Escape) {
+                this.IsInEditMode = false;
+                Text = oldText;
+                e.Handled = true;
+                _cancelled = true;
+            }
+        }
+
+        
+
+        public event EditingCancelledHandler EditingCancelled;
+
+        public event EditingCompleteHandler EditingComplete;
+
+        #endregion Event Handlers
+
+        #region Events
+
+        public delegate void EditingCompleteHandler(object sender, string text);
+
+        public delegate void EditingCancelledHandler(object sender, string oldtext);
+
+        #endregion
+
+    }
+
+}
---
+++ BioLink.Taxa/TaxonNameParser.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BioLink.Client.Utilities;
+
+namespace BioLink.Client.Taxa {
+
+    public static class TaxonNameParser {
+
+        public static TaxonName ParseName(TaxonViewModel taxon, string str) {
+            
+
+            if (str == null || String.IsNullOrEmpty(str.Trim())) {
+                return null;
+            }
+
+            str = str.Trim();
+
+            TaxonName name = new TaxonName();
+            if (taxon.LiteratureName.ValueOrFalse() || str.IndexOf(' ') < 0) {
+                name.Epithet = str.Trim();
+                return name;
+            }
+
+            str = str.Replace(", ", " ");
+            str = str.Replace(",", " ");
+            string[] bits = str.Split(' ');
+            string lastWord = bits[bits.Length - 1];
+            if (lastWord.IsNumeric()) {
+                name.Year = lastWord;
+                bits[bits.Length - 1] = "";
+                if (name.Year.EndsWith(")")) {
+                    name.ChangeCombination = true;
+                    name.Year = name.Year.Substring(0, name.Year.Length - 1);
+                }
+                if (bits.Length > 1) {
+                    if (taxon.AvailableName.ValueOrFalse() || String.IsNullOrEmpty(taxon.ElemType)) {
+                        // For available names assume the last word is the author
+                        name.Author = bits[bits.Length - 2];
+                        bits[bits.Length - 2] = "";
+                    } else {
+                        // For valid names assume all other words, other then the first, are part of the author
+                        for (int i = 1; i < bits.Length - 1; ++i) {
+                            name.Author += bits[i] + " ";
+                        }
+                        name.Author = taxon.Author.Trim();
+                    }
+                    if (name.Author.StartsWith("(")) {
+                        name.Author = taxon.Author.Substring(1);
+                    }
+                }
+            } else {
+                // Determine if the last word starts with a capital
+                // if so, then it is the author, otherwise its a Genus or other
+                char firstLetter = lastWord[0];
+                if (firstLetter == '(' && (lastWord.Length > 1)) {
+                    firstLetter = lastWord[1];
+                }
+                if (Char.IsUpper(firstLetter)) {
+                    name.Author = lastWord;
+                    bits[bits.Length - 1] = "";
+                    if (name.Author.StartsWith("(") && name.Author.EndsWith(")")) {
+                        name.Author = name.Author.Substring(1, name.Author.Length - 2);
+                        name.ChangeCombination = true;
+                    }
+                }
+            }
+            
+            if (taxon.AvailableName.ValueOrFalse() || String.IsNullOrEmpty(taxon.ElemType)) {
+                // If the item is an available name, the rest of the text is the taxon name
+                name.Epithet = "";
+                foreach (string bit in bits) {
+                    name.Epithet = (name.Epithet + " " + bit).Trim();
+                }
+            } else {
+                // This is a valid name, so the epithet should be the first non-blank portion of the name
+                name.Epithet = bits[0].Trim();
+                if (name.Epithet.StartsWith("(") && name.Epithet.EndsWith(")")) {
+                    name.Epithet = name.Epithet.Substring(1, name.Epithet.Length - 2);
+                }
+            }
+
+            return name;
+        }
+    }
+
+    public class TaxonName {
+        public string Epithet { get; set; }
+        public string Author { get; set; }
+        public string Year { get; set; }
+        public bool ChangeCombination { get; set; }
+    }
+}
