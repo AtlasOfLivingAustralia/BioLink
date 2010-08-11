68008f6d4f56a8e550644d476a94ae604ceadbbf
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2010-08-11 23:04:27 +0000

    added missing files from yesterdays commit

---
+++ BioLinkApplication/Styles.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace BioLinkApplication {
+
+    partial class Styles : ResourceDictionary {
+
+        public Styles() {
+            InitializeComponent();
+        }
+
+        public void CloseButtonClicked(object sender, RoutedEventArgs e) {
+
+            MessageBox.Show("Here");
+        }
+    }
+}
