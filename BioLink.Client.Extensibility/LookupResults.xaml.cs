10ea4948dc4bddc79b181d5ffecd116dcc37d526
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-02-21 05:09:46 +0000

    Fixed issue #2

---
+++ BioLink.Client.Extensibility/LookupResults.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BioLink.Data;
+using BioLink.Data.Model;
+using BioLink.Client.Utilities;
+using BioLink.Client.Extensibility;
+
+namespace BioLink.Client.Extensibility {
+    /// <summary>
+    /// Interaction logic for LookupResults.xaml
+    /// </summary>
+    public partial class LookupResults : Window {
+
+        private List<LookupResult> _model;
+
+        public LookupResults(List<LookupResult> model) {
+            InitializeComponent();
+            _model = model;
+            lst.ItemsSource = _model;
+            lst.SelectedIndex = 0;
+            Loaded += new RoutedEventHandler(LookupResults_Loaded);
+        }
+
+        void LookupResults_Loaded(object sender, RoutedEventArgs e) {
+            Keyboard.Focus(lst);
+        }
+
+        public LookupResult SelectedItem {
+            get { return lst.SelectedItem as LookupResult; }
+        }
+
+        private void btnAccept_Click(object sender, RoutedEventArgs e) {
+            if (lst.SelectedItem != null) {
+                this.DialogResult = true;
+                this.Close();
+            }
+        }
+
+        private void lst_MouseDoubleClick(object sender, MouseButtonEventArgs e) {
+            if (lst.SelectedItem != null) {
+                this.DialogResult = true;
+                this.Close();
+            }
+        }
+
+    }
+}
---
+++ BioLinkDAL/Model/LookupResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BioLink.Data.Model {
+
+    public class LookupResult : BioLinkDataObject {
+
+        public int LookupObjectID { get; set; }
+        public string Label { get; set; }
+        public LookupType LookupType { get; set; }
+        
+        protected override System.Linq.Expressions.Expression<Func<int>> IdentityExpression {
+            get { return () => LookupObjectID; }
+        }
+    
+    }
+}
