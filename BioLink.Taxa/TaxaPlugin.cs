3b99a8e8e9d303741baefc446f4321d32941db47
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-11-08 00:53:56 +0000

    Further work on drag drop lookup controls. Taxa pinnables now display their full names

--- BioLink.Taxa/TaxaPlugin.cs
+++ BioLink.Taxa/TaxaPlugin.cs
@@ -15,6 +15,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Windows;
 using BioLink.Client.Extensibility;
 using BioLink.Client.Utilities;
@@ -33,9 +34,6 @@ namespace BioLink.Client.Taxa {
 
         private ControlHostWindow _regionExplorer;
 
-        public TaxaPlugin() {
-        }
-
         public override void  InitializePlugin(User user, PluginManager pluginManager, Window parentWindow) {
  	        base.InitializePlugin(user, pluginManager, parentWindow);
             _taxaService = new TaxaService(user);            
@@ -50,28 +48,21 @@ namespace BioLink.Client.Taxa {
         }
 
         public override List<IWorkspaceContribution> GetContributions() {            
-            List<IWorkspaceContribution> contrib = new List<IWorkspaceContribution>();
-
-            contrib.Add(new MenuWorkspaceContribution(this, "ShowExplorer", (obj, e) => { PluginManager.EnsureVisible(this, "TaxonExplorer"); },
-                String.Format("{{'Name':'View', 'Header':'{0}','InsertAfter':'File'}}", _R("Taxa.Menu.View")),
-                String.Format("{{'Name':'ShowTaxaExplorer', 'Header':'{0}'}}", _R("Taxa.Menu.ShowExplorer"))
-            ));
-
-            contrib.Add(new MenuWorkspaceContribution(this, "ShowDistributionRegionExplorer", (obj, e) => { ShowRegionExplorer(); },
-                String.Format("{{'Name':'View', 'Header':'{0}','InsertAfter':'File'}}", _R("Taxa.Menu.View")),
-                String.Format("{{'Name':'ShowDistributionRegionExplorer', 'Header':'Show Distribution Region Explorer'}}")
-            ));
-
-            contrib.Add(new MenuWorkspaceContribution(this, "ShowXMLImport", (obj, e) => { ShowXMLImport(); },
-                "{'Name':'Tools', 'Header':'Tools','InsertAfter':'View'}",
-                "{'Name':'Import', 'Header':'Import'}",
-                "{'Name':'ShowXMLImport', 'Header':'XML Import'}"));
+            var contrib = new List<IWorkspaceContribution> {
+                                                               new MenuWorkspaceContribution(this, "ShowExplorer", (obj, e) => PluginManager.EnsureVisible(this, "TaxonExplorer"),
+                                                                                             String.Format("{{'Name':'View', 'Header':'{0}','InsertAfter':'File'}}", _R("Taxa.Menu.View")),
+                                                                                             String.Format("{{'Name':'ShowTaxaExplorer', 'Header':'{0}'}}", _R("Taxa.Menu.ShowExplorer"))
+                                                                   ), new MenuWorkspaceContribution(this, "ShowDistributionRegionExplorer", (obj, e) => ShowRegionExplorer(),
+                                                                                                    String.Format("{{'Name':'View', 'Header':'{0}','InsertAfter':'File'}}", _R("Taxa.Menu.View")),
+                                                                                                    String.Format("{{'Name':'ShowDistributionRegionExplorer', 'Header':'Show Distribution Region Explorer'}}")
+                                                                          ), new MenuWorkspaceContribution(this, "ShowXMLImport", (obj, e) => ShowXMLImport(),
+                                                                                                           "{'Name':'Tools', 'Header':'Tools','InsertAfter':'View'}",
+                                                                                                           "{'Name':'Import', 'Header':'Import'}",
+                                                                                                           "{'Name':'ShowXMLImport', 'Header':'XML Import'}")
+                                                           };
 
 
-            _explorer = new ExplorerWorkspaceContribution<TaxonExplorer>(this, "TaxonExplorer", new TaxonExplorer(this), _R("TaxonExplorer.Title"),
-                (explorer) => {
-                    explorer.InitialiseTaxonExplorer();
-                });
+            _explorer = new ExplorerWorkspaceContribution<TaxonExplorer>(this, "TaxonExplorer", new TaxonExplorer(this), _R("TaxonExplorer.Title"), explorer => explorer.InitialiseTaxonExplorer());
 
             contrib.Add(_explorer);
 
@@ -82,12 +73,8 @@ namespace BioLink.Client.Taxa {
             User.CheckPermission(PermissionCategory.IMPORT_MATERIAL, PERMISSION_MASK.ALLOW, "You do not have sufficient privileges to import into BioLink!");
 
             if (_xmlImportOptions == null) {
-                _xmlImportOptions = new XMLIOImportOptions();
-                _xmlImportOptions.Owner = PluginManager.Instance.ParentWindow;
-                _xmlImportOptions.WindowStartupLocation = WindowStartupLocation.CenterOwner;
-                _xmlImportOptions.Closed += new EventHandler((sender, args) => {
-                    _xmlImportOptions = null;
-                });
+                _xmlImportOptions = new XMLIOImportOptions {Owner = PluginManager.Instance.ParentWindow, WindowStartupLocation = WindowStartupLocation.CenterOwner};
+                _xmlImportOptions.Closed += (sender, args) => { _xmlImportOptions = null; };
             }
 
             _xmlImportOptions.Show();
@@ -95,8 +82,8 @@ namespace BioLink.Client.Taxa {
 
         public override bool RequestShutdown() {
             if (_explorer != null && _explorer.Content != null) {
-                TaxonExplorer explorer = _explorer.Content as TaxonExplorer;
-                if (explorer.AnyChanges()) {
+                var explorer = _explorer.Content as TaxonExplorer;
+                if (explorer != null && explorer.AnyChanges()) {
                     return explorer.Question(_R("TaxonExplorer.prompt.ShutdownDiscardChanges"), _R("TaxonExplorer.prompt.ConfirmAction.Caption"));
                 }
             }
@@ -107,7 +94,7 @@ namespace BioLink.Client.Taxa {
             base.Dispose();
             if (_explorer != null && _explorer.Content != null) {
 
-                if (Config.GetGlobal<bool>("Taxa.RememberExpandedTaxa", true)) {
+                if (Config.GetGlobal("Taxa.RememberExpandedTaxa", true)) {
                     List<string> expandedElements = GetExpandedParentages(_explorer.ContentControl.ExplorerModel);
                     if (expandedElements != null) {
                         Config.SetProfile(User, "Taxa.Explorer.ExpandedTaxa", expandedElements);
@@ -124,12 +111,12 @@ namespace BioLink.Client.Taxa {
         }
 
         public List<string> GetExpandedParentages(ObservableCollection<HierarchicalViewModelBase> model) {
-            List<string> list = new List<string>();
+            var list = new List<string>();
             ProcessList(model, list);
             return list;
         }
 
-        private void ProcessList(ObservableCollection<HierarchicalViewModelBase> model, List<string> list) {
+        private void ProcessList(IEnumerable<HierarchicalViewModelBase> model, List<string> list) {
             foreach (TaxonViewModel tvm in model) {
                 if (tvm.IsExpanded) {
                     list.Add(tvm.GetParentage());
@@ -144,7 +131,7 @@ namespace BioLink.Client.Taxa {
             if (pinnable != null && pinnable.LookupType == LookupType.Taxon) {
                 Taxon t = Service.GetTaxon(pinnable.ObjectID);
                 if (t != null) {
-                    TaxonViewModel m = new TaxonViewModel(null, t, _explorer.ContentControl.GenerateTaxonDisplayLabel);
+                    var m = new TaxonViewModel(null, t, tt => tt.TaxaFullName);
                     return m;
                 }                
             }
@@ -167,51 +154,35 @@ namespace BioLink.Client.Taxa {
                 return list;
             }
 
-            var obj = selected[0] as TaxonViewModel;
-            if (obj != null) {
-
-                var taxon = obj as TaxonViewModel;
-
+            var taxon = selected[0] as TaxonViewModel;
+            if (taxon != null) {
                 
-                list.Add(new Command("Show in explorer", (dataobj) => { _explorer.ContentControl.ShowInExplorer(taxon.TaxaID); }));
+                list.Add(new Command("Show in explorer", dataobj => _explorer.ContentControl.ShowInExplorer(taxon.TaxaID)));
 
-                var reports = GetReportsForTaxon(selected.ConvertAll((vm) => { return vm as TaxonViewModel; }));
+                var reports = GetReportsForTaxon(selected.ConvertAll(vm => vm as TaxonViewModel));
                 if (reports.Count > 0) {
                     list.Add(new CommandSeparator());
-                    foreach (IBioLinkReport loopreport in reports) {
-                        var report = loopreport;
-                        list.Add(new Command(report.Name, (dataobj) => {
-                            _explorer.ContentControl.RunReport(report);
-                        }));
-                    }
+                    list.AddRange(reports.Select(report => new Command(report.Name, dataobj => _explorer.ContentControl.RunReport(report))));
                 }
 
                 list.Add(new CommandSeparator());
-                list.Add(new Command("Edit Name...", (dataobj) => { _explorer.ContentControl.EditTaxonName(taxon); }));
-                list.Add(new Command("Edit Details...", (dataobj) => { _explorer.ContentControl.EditTaxonDetails(taxon.TaxaID); }));
+                list.Add(new Command("Edit Name...", dataobj => _explorer.ContentControl.EditTaxonName(taxon)));
+                list.Add(new Command("Edit Details...", dataobj => _explorer.ContentControl.EditTaxonDetails(taxon.TaxaID)));
             }
 
             if (selected[0] is DistributionRegionViewModel) {
                 var region = selected[0] as DistributionRegionViewModel;
-                list.Add(new Command("Taxa for Distribution Region Report", (dataobj) => {
-                    PluginManager.Instance.RunReport(this, new TaxaForDistributionRegionReport(User, region.Model));
+                list.Add(new Command("Taxa for Distribution Region Report", dataobj => {
+                    if (region != null) {
+                        PluginManager.Instance.RunReport(this, new TaxaForDistributionRegionReport(User, region.Model));
+                    }
                 }));
             }
             return list;
         }
 
         public List<IBioLinkReport> GetReportsForTaxon(List<TaxonViewModel> taxa) {
-            List<IBioLinkReport> list = new List<IBioLinkReport>();
-
-            
-            list.Add(new MaterialForTaxonReport(User, taxa[0]));
-            list.Add(new TypeListReport(User, taxa[0]));
-            list.Add(new TaxaAssociatesReport(User, taxa));
-            list.Add(new SiteForTaxaReport(User, taxa[0]));
-            list.Add(new ChecklistReport(User, taxa[0]));
-            list.Add(new TaxonStatisticsReport(User, taxa[0]));
-            list.Add(new MultimediaReport(User, taxa[0], TraitCategoryType.Taxon));
-
+            var list = new List<IBioLinkReport> {new MaterialForTaxonReport(User, taxa[0]), new TypeListReport(User, taxa[0]), new TaxaAssociatesReport(User, taxa), new SiteForTaxaReport(User, taxa[0]), new ChecklistReport(User, taxa[0]), new TaxonStatisticsReport(User, taxa[0]), new MultimediaReport(User, taxa[0], TraitCategoryType.Taxon)};
 
             return list;
         }
@@ -227,19 +198,17 @@ namespace BioLink.Client.Taxa {
 
         public override void Select<T>(LookupOptions options, Action<SelectionResult> success) {
             var t = typeof(T);
-            IHierarchicalSelectorContentProvider selectionContent = null;
+            IHierarchicalSelectorContentProvider selectionContent;
             if (typeof(Taxon).IsAssignableFrom(t)) {
                 selectionContent = new TaxonSelectorContentProvider(User, _explorer.Content as TaxonExplorer, options);
             } else {
                 throw new Exception("Unhandled Selection Type: " + t.Name);
             }
 
-            if (selectionContent != null) {
-                var frm = new HierarchicalSelector(User, selectionContent, success);
-                frm.Owner = ParentWindow;
-                frm.WindowStartupLocation = System.Windows.WindowStartupLocation.CenterOwner;
-                frm.ShowDialog();
-            }
+
+            var frm = new HierarchicalSelector(User, selectionContent, success) {Owner = ParentWindow, WindowStartupLocation = WindowStartupLocation.CenterOwner};
+            frm.ShowDialog();
+         
         }
 
         public override bool CanEditObjectType(LookupType type) {
@@ -248,7 +217,10 @@ namespace BioLink.Client.Taxa {
 
         public override void EditObject(LookupType type, int objectID) {
             if (type == LookupType.Taxon) {
-                (_explorer.Content as TaxonExplorer).EditTaxonDetails(objectID);
+                var taxonExplorer = _explorer.Content as TaxonExplorer;
+                if (taxonExplorer != null) {
+                    taxonExplorer.EditTaxonDetails(objectID);
+                }
             }
         }
 
@@ -269,10 +241,10 @@ namespace BioLink.Client.Taxa {
                 var explorer = new DistributionRegionExplorer(this, User);
                 _regionExplorer = PluginManager.Instance.AddNonDockableContent(this, explorer, "Distribution Region Explorer", SizeToContent.Manual);
 
-                _regionExplorer.Closed += new EventHandler((object sender, EventArgs e) => {
-                    _regionExplorer.Dispose();
-                    _regionExplorer = null;
-                });
+                _regionExplorer.Closed += (sender, e) => {
+                                              _regionExplorer.Dispose();
+                                              _regionExplorer = null;
+                                          };
             }
 
             if (_regionExplorer != null) {
@@ -292,8 +264,8 @@ namespace BioLink.Client.Taxa {
 
         public IllegalTaxonMoveException(Taxon source, Taxon dest, string message)
             : base(message) {
-            this.SourceTaxon = source;
-            this.DestinationTaxon = dest;
+            SourceTaxon = source;
+            DestinationTaxon = dest;
         }
 
         public Taxon SourceTaxon { get; private set; }
