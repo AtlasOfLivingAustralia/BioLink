86ab42d9121de51a93a216d0de8ea963db19cb46
david.baird.ala@gmail.com <david.baird.ala@gmail.com@09e1001a-9679-c032-a9e2-1eb070a1449d>
2014-05-24 12:09:52 +0000

    more on transform

---
+++ BioLink.Client.Extensibility/Transform/EditTransformationPipeline.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BioLink.Client.Utilities;
+using BioLink.Client.Extensibility;
+using BioLink.Data;
+
+namespace BioLink.Client.Extensibility.Transform {
+    /// <summary>
+    /// Interaction logic for EditTransformationPipeline.xaml
+    /// </summary>
+    public partial class EditTransformationPipeline : Window {
+
+        public EditTransformationPipeline() {
+            InitializeComponent();
+        }
+
+        public EditTransformationPipeline(TransformationPipline transform) {
+            InitializeComponent();
+            this.TransformationPipeline = transform;
+            RenderTransforms();
+        }
+
+        private void RenderTransforms() {
+            transformersPanel.Children.Clear();
+            if (TransformationPipeline != null) {
+                foreach (IValueTransformer t in TransformationPipeline.Transformers) {
+                    var tt = new ValueTransformerControl(t);
+                    transformersPanel.Children.Add(tt);
+                }
+            }
+        }
+
+
+        public TransformationPipline TransformationPipeline { get; private set; }
+
+        public User User {
+            get { return PluginManager.Instance.User; }
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void btnOK_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+    }
+}
